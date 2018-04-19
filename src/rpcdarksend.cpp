b2342cf97509f4179507c7046231d1453a169cdb
Aliyul Erfan <erfan007p@gmail.com>
2018-04-20 02:06:53 +0700

    .

--- src/rpcdarksend.cpp
+++ src/rpcdarksend.cpp
@@ -317,7 +317,7 @@ UniValue masternode(const UniValue& params, bool fHelp)
                                mn.addr.ToString();
                 std::string strFull = streamFull.str();
                 obj.push_back(Pair(mn.vin.prevout.hash.ToString().c_str(), strFull));
-
+            }
         }
         return obj;
     }
